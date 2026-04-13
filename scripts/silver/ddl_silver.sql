1dc97286f552a4b0dc43034bfc3d78504340ec5f
saikrishna4512 <85178810+saikrishna4512@users.noreply.github.com>
2026-04-12 21:18:47 -0700

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -22,7 +22,7 @@ cst_id int,
 cst_key nvarchar(50),
 cst_firstname nvarchar(50),
 cst_lastname nvarchar(50),
-cst_material_status nvarchar(50),
+cst_marital_status nvarchar(50),
 cst_gndr nvarchar(50),
 cst_create_date date,
 dwh_create_date datetime2 default getdate()

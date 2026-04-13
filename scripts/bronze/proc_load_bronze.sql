2ef1482db7b15f2075320d75012ff9a2784c58ce
saikrishna4512 <85178810+saikrishna4512@users.noreply.github.com>
2026-04-12 21:51:59 -0700

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -43,7 +43,7 @@ begin
 
 			 PRINT '>> Inserting data into: bronze.crm_cust_info';
              BULK INSERT bronze.crm_cust_info
-              FROM 'D:\Azure Portfolio Projects\SQL Data Warehousing Project\Data\Crm\cust_info.csv'
+              FROM 'datasets/source_crm/cust_info.csv'
               WITH (
                FIRSTROW=2,
                FIELDTERMINATOR= ',',
@@ -60,7 +60,7 @@ begin
 
 			 PRINT '>> Inserting data into: bronze.crm_prd_info';
              BULK INSERT bronze.crm_prd_info
-              FROM 'D:\Azure Portfolio Projects\SQL Data Warehousing Project\Data\Crm\prd_info.csv'
+              FROM 'datasets/source_crm/prd_info.csv'
               WITH (
                FIRSTROW=2,
                FIELDTERMINATOR= ',',
@@ -76,7 +76,7 @@ begin
 
 			   PRINT '>> Inserting data into: bronze.crm_sales_details';
              BULK INSERT bronze.crm_sales_details
-              FROM 'D:\Azure Portfolio Projects\SQL Data Warehousing Project\Data\Crm\sales_details.csv'
+              FROM 'datasets/source_crm/sales_details.csv'
               WITH (
                FIRSTROW=2,
                FIELDTERMINATOR= ',',
@@ -96,7 +96,7 @@ begin
 
             PRINT '>> Inserting data into: bronze.erp_cust_az12';
              BULK INSERT bronze.erp_cust_az12
-              FROM 'D:\Azure Portfolio Projects\SQL Data Warehousing Project\Data\ERP\CUST_AZ12.csv'
+              FROM 'datasets/source_erp/CUST_AZ12.csv'
               WITH (
                FIRSTROW=2,
                FIELDTERMINATOR= ',',
@@ -113,7 +113,7 @@ begin
 
 			 PRINT '>> Inserting data into: bronze.erp_loc_a101';
              BULK INSERT bronze.erp_loc_a101
-              FROM 'D:\Azure Portfolio Projects\SQL Data Warehousing Project\Data\ERP\LOC_A101.csv'
+              FROM 'datasets/source_erp/LOC_A101.csv'
               WITH (
                FIRSTROW=2,
                FIELDTERMINATOR= ',',
@@ -130,7 +130,7 @@ begin
 
 			  PRINT '>> Inserting data into: bronze.erp_px_cat_g1v2';
              BULK INSERT bronze.erp_px_cat_g1v2
-              FROM 'D:\Azure Portfolio Projects\SQL Data Warehousing Project\Data\ERP\PX_CAT_G1V2.csv'
+              FROM 'datasets/source_erp/PX_CAT_G1V2.csv'
               WITH (
                FIRSTROW=2,
                FIELDTERMINATOR= ',',
